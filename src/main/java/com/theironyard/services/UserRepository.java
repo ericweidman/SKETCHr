44b298f7f8b123b55b0755d4839a76841a0b9284
ericweidman <ericweidman@gmail.com>
2016-04-05 12:34:04 -0400

    Initial Import

--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 /**
  * Created by ericweidman on 4/3/16.
  */
-public interface UserRepository extends CrudRepository<User, Integer>
-
-{
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
