b50121a8569ad27b2b91f8163f1ced6db3baf7a9
Patrick Coleman <colemanrpatrick@gmail.com>
2016-04-08 10:49:43 -0400

    GET R DONE

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 var canvasApp ={
   urls:{
     canvasIMG: '/upload',
+    getCanvasImg: '/photo/',
+    canvasGallery: '/gallery/'
   }
 };
 function saveCanvasImg(canvasString){
@@ -15,6 +17,39 @@ function saveCanvasImg(canvasString){
   });
 }
 
+var id = '33';
+
+function getGallery(){
+  $.ajax({
+    url:canvasApp.urls.canvasGallery,
+    method:'GET',
+    success: function(){
+      console.log('here they are',canvasApp.urls.canvasGallery);
+      // alert('it worked!');
+    }
+  });
+}
+
+
+
+
+function getCanvasImg(canvasIMGID){
+  $.ajax({
+    method:'GET',
+    url:canvasApp.urls.getCanvasImg + id,
+    success: function(canvasIMG){
+      var img = new Image();
+      var canvasGet = decodeURIComponent(canvasIMG.fileName).split('=')[1].substr(1);
+      console.log('got it 2', canvasGet);
+      // console.log('here it is again',canvasGet);
+      img.src = decodeURIComponent(canvasGet);
+      // console.log('and decoded',img);
+      document.body.appendChild(img);
+      alert('gotIT!');
+    }
+  });
+}
+
 document.getElementById('save').addEventListener('click',function(){
   var canvasDATA = canvas.toDataURL(0, 0, context.canvas.width, context.canvas.height);
   var canvasString = JSON.stringify(canvasDATA);
@@ -22,17 +57,13 @@ document.getElementById('save').addEventListener('click',function(){
   saveCanvasImg(canvasString);
 });
 
-function getCanvasImg(){
-  $.ajax({
-    method:'GET',
-    url:'/photo/5',
-    success: function(canvasIMG){
-      // console.log(canvasDATA);
-      console.log(canvasIMG);
-      alert('gotIT!');
-    }
-  });
-}
+
 document.getElementById('open').addEventListener('click', function(){
-  $('openTEST').append('canvasIMG');
+  getCanvasImg();
+  getGallery();
+
+  // var img = new Image();
+  // img.src = decodeURIComponent(canvasIMG);
+  // console.log(window.glob.fileName);
+  // $('openTEST').append(canvasIMG);
 });
