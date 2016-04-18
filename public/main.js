6db992fe398ae076d8f3d894486c5a80ecefbef8
Patrick Coleman <colemanrpatrick@gmail.com>
2016-04-18 13:57:43 -0400

    COMMMENTS

--- public/main.js
+++ public/main.js
@@ -94,9 +94,10 @@ function getGallery(){
         console.log(namer);
         var img = new Image();
         img.src = enc;
-        $('.gallerySpace').prepend(img);
-        $('.gallerySpace').prepend('<p data-image-id="' + element.id +'">' + namer+' '+'created by '+arter+'</p>');
-        $('.gallerySpace').prepend('<input type=text class="comment"/><button type="submit" class="critique"/>');
+
+        $('.gallerySpace').prepend('<article data-image-id="'+element.id+'"><p>' + namer+' '+'created by '+arter+'</p><input type=text class="comment"/><button type="button" class="critique">Add Comment</button></article>');
+        $('[data-image-id=' + element.id+']').prepend(img);
+
       });
     }
   });
@@ -105,11 +106,11 @@ function getGallery(){
 $('body').on('click','.critique',function(event){
   event.preventDefault();
   console.log("WHAT AM I",this)
-  var id = $(this).siblings('p').data('image-id');
-  console.log("ID IS THIS", $(this).siblings('p').data('image-id'));
-  var com = $('input[class="comment"]').val();
-  console.log('comment sent');
-  postComment(com,id);
+  var id = parseInt($(this).closest('article').data('image-id'));
+  console.log("ID IS THIS", $(this).closest('article').data('image-id'));
+  var comment = $(this).closest('article').find('input[class="comment"]').val();
+  console.log('comment sent', comment);
+  postComment(comment,id);
 });
 
 
@@ -121,7 +122,7 @@ function postComment(com,id){
   //  contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     data: {
-      com
+      com: JSON.stringify(com)
     },
     success: function(){
       console.log('this comment',com);
